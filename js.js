16b908ccced7e641dd4ea45df697d7a30f3c0d16
Nicole Cruz <nickruz7301@hotmail.com>
2024-10-17 21:17:57 -0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,18 @@
+// Modo noturno
+const toggleDarkMode = document.querySelector("#toggle");
+let isDarkMode = false;
+
+toggleDarkMode.addEventListener("click", () => {
+    isDarkMode = !isDarkMode;
+    // Alterna entre os modos
+    document.documentElement.setAttribute('data-theme', isDarkMode ? 'dark' : '');
+});
+
+// Seleciona o ícone do menu hambúrguer e a lista do menu
+const menuHamburguer = document.getElementById('menu-hamburguer');
+const menuLista = document.querySelector('.cabecalho-lista');
+
+// Adiciona um evento de clique para mostrar/ocultar o menu
+menuHamburguer.addEventListener('click', () => {
+    menuLista.classList.toggle('active');
+});
